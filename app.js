4b3603ce730adfea4dfcb98f53eca29f197c36b5
omzeton <adamr6561@gmail.com>
2019-07-10 15:50:27 +0200

    Adding process.env.PORT again...

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
-    const PORT = 8080;
+    const PORT = process.env.PORT || 8080;
     const server = app.listen(PORT);
     const io = require("./socket").init(server);
     io.on("connection", socket => {
